de0b83d6804bcc59cfec01843a69f2bf070261a2
A≡Azryel <mmsantos@carc.pshs.edu.ph>
2026-09-22 15:22:05 +0800

    Delete moneis.py

--- moneis.py
+++
@@ -1,6 +0,0 @@
-vawid_inpwuts = ["cash", "gcash", "card"]
-check = print(input("Select payment method. (cash, gcash, card)"))
-if check in vawid_inpwuts:
-    print("Payment method is valid")
-else:
-    print("Payment method is not valid")
